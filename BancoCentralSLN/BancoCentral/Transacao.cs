e7cb369fd47aae6da933164c201c04dc429bc33b
LaraFabianD <lara.f.dantas240@gmail.com>
2026-02-23 08:36:17 -0300

    commit recente

--- BancoCentralSLN/BancoCentral/Transacao.cs
+++ BancoCentralSLN/BancoCentral/Transacao.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BancoCentral
-{
+namespace BancoCentral;
     internal class Transacao
     {
         public decimal Valor { get; set; }
         public DateTime Data { get; set; }
         public string Tipo { get; set; }
-        public  ContaBancaria ContaOrigem { get; set; }
+        public ContaBancaria ContaOrigem { get; set; }
         public ContaBancaria ContaDestino { get; set; }
-
+        //rr 
+        private decimal _valor;
+        public decimal valor
+        {
+            get { return _valor; }
+            set
+            {
+                if (value > 0)
+                    _valor = value;
+                else
+                    Console.WriteLine("Erro: o valor da transaçãodeve ser positivo");
+            }
+        }
     }
-}
+
+
+
