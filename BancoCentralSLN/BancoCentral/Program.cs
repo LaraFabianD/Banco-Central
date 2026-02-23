13f8abe2d52d1c094107b7ef43557561eabe0d19
LaraFabianD <lara.f.dantas240@gmail.com>
2026-02-23 09:17:12 -0300

    commitAgr

--- BancoCentralSLN/BancoCentral/Program.cs
+++ BancoCentralSLN/BancoCentral/Program.cs
@@ -1,40 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using BancoCentral;
+using static BancoCentral.Transacao;
 
-Console.WriteLine("Hello, World!");
-static void Main(string[] args)
+class Program
 {
-    //Criando o projeto bancoaaaa
-    Banco meuBanco = new Banco();
-    meuBanco.Nome = "Banco do Brasil";
-    meuBanco.CodigoBACEN = "001";
+    static void Main(string[] args)
+    {
+        TransacaoPix meuPix = new TransacaoPix();
+        meuPix.Valor = 15000;
+        meuPix.ChavePix = "contato@gmail.com";
 
-    //Conta Bancária
-    ContaBancaria contaOrigem = new ContaBancaria();
-    contaOrigem.Agencia = "1234";
-    contaOrigem.NumeroConta = "56789-0";
+        Transacaoted meuTed = new Transacaoted();
+        meuTed.Valor = 6000;
 
-    ContaBancaria contaDestino = new ContaBancaria();
-    contaDestino.Agencia = "4321";
-    contaDestino.NumeroConta = "98765-0";
-
-    //Criando o objeto Transação
-    Transacao minhaTransacao = new Transacao();
-    minhaTransacao.Valor = 250.00m;
-    minhaTransacao.Data = DateTime.Now;
-    minhaTransacao.Tipo = "PIX";
-    minhaTransacao.ContaOrigem = contaOrigem;
-    minhaTransacao.ContaDestino = contaDestino;
-
-    //Exibindo os dados na tela
-    Console.WriteLine("---REGISTRO BACEN---");
-    Console.WriteLine("Banco: " + meuBanco.Nome);
-    Console.WriteLine("Código: " + meuBanco.CodigoBACEN);
-    Console.WriteLine("------------------------");
-    Console.WriteLine("Conta Origem: " + contaOrigem.NumeroConta + " (Agência " + contaOrigem.Agencia + ")");
-    Console.WriteLine("Conta Destino: " + contaDestino.NumeroConta + " (Agência " + contaDestino.Agencia + ")");
-    Console.WriteLine("Tipo: " + minhaTransacao.Tipo);
-    Console.WriteLine("Valor: R$ " + minhaTransacao.Valor);
-    Console.WriteLine("Data: " + minhaTransacao.Data);
-
+        Console.WriteLine($"Pix de R$ {meuPix.Valor} é válido? {meuPix.Validar()}");
+        Console.WriteLine($"TED de R$ {meuTed.Valor} é válido? {meuTed.Validar()}");
+    }
 }
